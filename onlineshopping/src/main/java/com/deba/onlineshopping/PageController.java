1443c10f90e26100850c09e058ec8bb40ea15b2e
debaprad <debasish.it.12@gmail.com>
2017-08-29 01:06:04 +0530

    sucessfully loaded landing page with static resorces & unstyled templet

--- onlineshopping/src/main/java/com/deba/onlineshopping/PageController.java
+++ onlineshopping/src/main/java/com/deba/onlineshopping/PageController.java
@@ -1,7 +1,9 @@
 package com.deba.onlineshopping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,4 +17,38 @@ public class PageController {
 		mv.addObject("greeting","Welcome to OnlineShopping !");
 		return mv;
 	}
+	
+	/*---------------diff between @RequestParam @PathVariable  starts  Not part of the project -------------------*/
+	
+	/*http://localhost:8989/onlineshopping/testRequestParam
+	 * required=false if we remove then the url should be contain query string http://localhost:8989/onlineshopping/testRequestParam?greeting=Welcome
+	*/	
+	@RequestMapping(value ="/testRequestParam")
+	public ModelAndView testRequestParam(@RequestParam(value="greeting",required=false) String greating)
+	{
+		if(greating== null || greating.isEmpty())
+		{
+			greating="Hello";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greating);
+		return mv;
+	}
+	
+	/*http://localhost:8989/onlineshopping/testPathVariable/Welcome
+	 * 
+	*/
+	@RequestMapping(value ="/testPathVariable/{greating}")
+	public ModelAndView testPathVariable(@PathVariable(value="greating",required=false) String greating)
+	{
+		if(greating== null || greating.isEmpty())
+		{
+			greating="Hello";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greating);
+		return mv;
+	}
+	
+	/*---------------diff between @RequestParam @PathVariable  ends -------------------*/
 }
